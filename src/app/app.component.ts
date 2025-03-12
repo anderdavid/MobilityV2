d1fe3a8f29928f41d99bcb0e0bb60bb92bf6f17b
anderson <anderdavid86@gmail.com>
2025-03-12 12:34:01 -0500

    se implementa routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UsersModule } from './users/users.module';
+import { LoginComponent } from './users/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UsersModule, LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'MobilityV2';
